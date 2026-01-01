64c51ba58bf959c12a66efce2d03f3ef358f8b3d
DunnEula <138508734+Dounogox@users.noreply.github.com>
2026-01-01 11:15:04 +0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,10 +1,11 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyBUYgQFuS-bMkF7BG_6peMOmpv1tFhiMP0"
+API_KEY = ""
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-2.0-flash")
 chat = model.start_chat()
 
 def ask_ai(prompt: str):
     response = chat.send_message(prompt)
+
     return response.text
